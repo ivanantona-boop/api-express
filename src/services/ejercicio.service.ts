fc818a2c332e54369c5b2ccfd43ec57c79e28cc6
Cloud District <clouddistrict@MacBook-Pro-de-Cloud.local>
2025-12-24 10:47:02 +0100

    servicio terminado

--- src/services/ejercicio.service.ts
+++ src/services/ejercicio.service.ts
@@ -1,5 +1,27 @@
 import { EjercicioRepository } from "../repository/ejercicio.repository";
 import { Ejercicio } from "../models/ejercicio.model";
+import { promises } from "dns";
 
 //instancio el repositorio para usar sus métodos
-const ejercicioRepo = new EjercicioRepository(); 
+const ejercicioRepo = new EjercicioRepository();
+
+export class EjercicioService {
+
+    async getAllEjercicios(): Promise<Ejercicio[]> {
+        return await ejercicioRepo.getAllEjercicios();
+    }
+
+    async creacionDeEjercicio(data: Ejercicio): Promise<Ejercicio> {
+        // Aquí podríamos poner lógica de negocio extra antes de guardar
+        return await ejercicioRepo.createEjercicio(data);
+    }
+    
+    async actualizarEjercicio(id: number, data: Ejercicio): Promise<Ejercicio | null> {
+        // Aquí podríamos poner lógica de negocio extra antes de actualizar
+        return await ejercicioRepo.updateEjercicio(id, data);
+    }
+
+    async eliminarEjercicio(id: number): Promise<void> {
+        return await ejercicioRepo.deleteEjercicio(id);
+    }
+}
